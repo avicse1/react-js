c186782b614ed2d9da92cf663480137e51883e0c
Avinash Kumar <avinash@ahaloans.com>
2021-09-01 16:26:12 +0530

    css to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    return (
+        <div className="expense-item">
+            <div>Sept 01th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$390.14</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
