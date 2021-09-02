922c7ba51180a3dc9c5533a759ca975d1505545b
Avinash Kumar <avinash@ahaloans.com>
2021-09-02 13:34:13 +0530

    seperate component for Date and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(pros) {
-    const month = pros.date.toLocaleString("en-US", { month: "long" });
-    const day = pros.date.toLocaleString("en-US", { day: "2-digit" });
-    const year = pros.date.getFullYear();
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={pros.date} />
             <div className="expense-item__description">
                 <h2>{pros.title}</h2>
                 <div className="expense-item__price">${pros.amount}</div>
