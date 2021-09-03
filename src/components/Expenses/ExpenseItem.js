08516678651e06cbcfb079d5058ff2588848b83e
Avinash Kumar <avicse509@gmail.com>
2021-09-03 12:41:57 +0530

    update text onclick button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,18 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (pros) => {
+    let title = pros.title;
+    function clickHandler() {
+        title = "updated!";
+        console.log(title);
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={pros.date} />
             <div className="expense-item__description">
-                <h2>{pros.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${pros.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     );
