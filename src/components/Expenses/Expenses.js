9d01e8e07f28531425bcee30146f3610e8a6c457
Avinash Kumar <avinash@ahaloans.com>
2021-09-03 11:00:30 +0530

    organize component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
+
 function Expenses(pros) {
     return (
         <Card className="expenses">
