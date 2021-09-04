893769d465e7793759dd40ce87711e76aab572e3
Avinash Kumar <avicse509@gmail.com>
2021-09-04 13:13:25 +0530

    Seperat compnent for ExpenseList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import { useState } from "react";
-
+import ExpensesList from "./ExpensesList";
 // Arrow function
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2021");
@@ -16,24 +15,13 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expneseContent = <p>No expense found</p>;
-    if (filterExpenses.length > 0) {
-        expneseContent = filterExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
     return (
         <Card className="expenses">
             <ExpenseFilter
                 selected={filteredYear}
                 onFilterChange={filterChangeHandler}
             />
-            {expneseContent}
+            <ExpensesList items={filterExpenses} />
         </Card>
     );
 };
